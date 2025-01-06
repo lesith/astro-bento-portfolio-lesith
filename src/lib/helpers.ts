105ebdc36c6a6608ba9e49dc61692e9cf52c8a63
Lesith Dissanayaka <lesithdissanayaka@gmail.com>
2025-01-05 23:45:04 -0500

    updates

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -7,8 +7,9 @@ export function getCurrentTimeInItaly(): Date {
   const now = new Date();
 
   // Convert the UTC time to Italy's time
-  const offsetItaly = 2; // Italy is in Central European Summer Time (UTC+2), but you might need to adjust this based on Daylight Saving Time
-  now.setHours(now.getUTCHours() + offsetItaly);
+  // const offsetItaly = 2; // Italy is in Central European Summer Time (UTC+2), but you might need to adjust this based on Daylight Saving Time
+  const offsetCanada = -5;
+  now.setHours(now.getUTCHours() + offsetCanada);
 
   return now;
 }
@@ -19,14 +20,14 @@ export function formatTimeForItaly(date: Date): string {
     minute: "2-digit",
     second: "2-digit",
     hour12: true, // This will format the time in 12-hour format with AM/PM
-    timeZone: "Europe/Rome",
+    timeZone: "America/Toronto",
   };
 
   let formattedTime = new Intl.DateTimeFormat("en-US", options).format(date);
 
   // Append the time zone abbreviation. You can automate this with libraries like `moment-timezone`.
   // For simplicity, here I'm just appending "CET", but do remember that Italy switches between CET and CEST.
-  formattedTime += " CET";
+  formattedTime += " EST";
 
   return formattedTime;
 }
